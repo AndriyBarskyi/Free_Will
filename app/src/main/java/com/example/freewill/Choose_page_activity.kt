844217ff62b731b300a490d1398ea2167fdd8bfe
MHatalyak <matronahu@gmail.com>
2022-04-11 20:35:44 -0700

    Welcome choose page

---
+++ app/src/main/java/com/example/freewill/Choose_page_activity.kt
@@ -0,0 +1,11 @@
+package com.example.freewill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Choose_page_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_page)
+    }
+}
