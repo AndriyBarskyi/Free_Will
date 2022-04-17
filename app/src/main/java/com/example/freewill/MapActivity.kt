7fb9d877b8cf60999181c480fa1b2e20dace821b
Andriy Barskyi <barskyiandriy@gmail.com>
2022-04-17 12:27:18 +0300

    fix multiple bugs

--- app/src/main/java/com/example/freewill/MapActivity.kt
+++ app/src/main/java/com/example/freewill/MapActivity.kt
@@ -8,6 +8,5 @@ class MapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
-
     }
 }
