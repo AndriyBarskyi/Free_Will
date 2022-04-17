104a024915d64aea3fae4d52b85d0d9387cac0a7
MHatalyak <matronahu@gmail.com>
2022-04-16 21:05:43 -0700

    schedule

---
+++ app/src/main/java/com/example/freewill/Check_email_entrance.kt
@@ -0,0 +1,11 @@
+package com.example.freewill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Check_email_entrance : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_email_entrance)
+    }
+}
