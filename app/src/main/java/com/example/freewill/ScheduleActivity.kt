6f15ced121f611aa294c7a9ab7c9081df0fa6292
Kron285 <91598400+Kron285@users.noreply.github.com>
2022-04-18 12:39:20 +0300

    navigation_drawer

--- app/src/main/java/com/example/freewill/ScheduleActivity.kt
+++ app/src/main/java/com/example/freewill/ScheduleActivity.kt
@@ -1,11 +1,62 @@
 package com.example.freewill
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.navigation.NavigationView
 
 class ScheduleActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schedule)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        val navView: NavigationView = findViewById(R.id.navView)
+        navView.setNavigationItemSelectedListener{
+            when(it.itemId){
+//                R.id.nav_schedule ->
+//                    Toast.makeText(applicationContext, "Schedule is clicked", Toast.LENGTH_SHORT).show()
+//
+//                R.id.nav_map ->
+//                    Toast.makeText(applicationContext, "Map is clicked", Toast.LENGTH_SHORT).show()
+//
+//                R.id.nav_settings ->
+//                    Toast.makeText(applicationContext, "Settings is clicked", Toast.LENGTH_SHORT).show()
+
+                R.id.nav_schedule -> startActivity(Intent(this, ScheduleActivity::class.java))
+
+                R.id.nav_map -> startActivity(Intent(this, MapActivity::class.java))
+
+                R.id.nav_settings ->
+                    Toast.makeText(applicationContext, "Settings is clicked", Toast.LENGTH_SHORT).show()
+
+            }
+            true
+        }
+
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
