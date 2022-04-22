3e48429ab209feddebb4731ea24b322c873d307a
barskyiandrii <barskyiandriy@gmail.com>
2022-04-22 17:42:45 +0300

    make popup foreground

--- app/src/main/java/com/example/freewill/MapActivity.kt
+++ app/src/main/java/com/example/freewill/MapActivity.kt
@@ -3,21 +3,20 @@ package com.example.freewill
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
-import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.PopupWindow
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.freewill.databinding.ActivityMapBinding
-import com.example.freewill.databinding.ActivitySearchPopupBinding
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
+
 
 class MapActivity : AppCompatActivity() {
 
@@ -28,8 +27,8 @@ class MapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
-
         drawerLayout = findViewById(R.id.drawerLayout)
+        drawerLayout.foreground.alpha = 0
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
 
         drawerLayout.addDrawerListener(toggle)
@@ -51,35 +50,25 @@ class MapActivity : AppCompatActivity() {
             true
         }
 
-
     }
 
-    @SuppressLint("ClickableViewAccessibility")
+    @SuppressLint("ClickableViewAccessibility", "InflateParams")
     fun onClickOpenPopUp(view:View) {
         mapBinding = ActivityMapBinding.inflate(layoutInflater)
         val inflater =
             view.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        drawerLayout = findViewById(R.id.drawerLayout)
+        drawerLayout.foreground.alpha = 120
         val popupView: View = inflater.inflate(R.layout.activity_search_popup, null)
-
-        //Specify the length and width through constants
         val width = LinearLayout.LayoutParams.MATCH_PARENT
         val height = LinearLayout.LayoutParams.MATCH_PARENT
-
-        //Make Inactive Items Outside Of PopupWindow
         val focusable = true
-
-        //Create a window with our parameters
         val popupWindow = PopupWindow(popupView, width, height, focusable)
 
-        //Set the location of the window on the screen
         popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
-
-
-
-        //Handler for clicking on the inactive zone of the window
-        popupView.setOnTouchListener { v, event -> //Close the window when clicked
-            popupWindow.dismiss()
-            true
+        popupWindow.isOutsideTouchable = true
+        popupWindow.setOnDismissListener {
+            drawerLayout.foreground.alpha = 0
         }
     }
 
