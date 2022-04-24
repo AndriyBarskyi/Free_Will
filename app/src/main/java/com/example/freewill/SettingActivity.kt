17721df5560eef5a04a785286a9b06b4d4519a38
Julian Zaiats <YULIAN.ZAIATS@lnu.edu.ua>
2022-04-24 18:12:13 +0300

    design#3

--- app/src/main/java/com/example/freewill/SettingActivity.kt
+++ app/src/main/java/com/example/freewill/SettingActivity.kt
@@ -1,11 +1,34 @@
 package com.example.freewill
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class SettingActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class  SettingActivity : AppCompatActivity() {
+    private val mLanguageCodeEn = "en"
+    private val mLanguageCodeUa = "ua"
+    protected override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+
+
+        //Change English to French when user clicked the button.
+        val a : Button = findViewById(R.id.english_language)
+        val b : Button = findViewById(R.id.ukraine_language)
+
+        b.setOnClickListener(View.OnClickListener { //Change Application level locale
+            LocaleHelper.setLocale(this, mLanguageCodeUa)
+            recreate()
+        })
+
+        a.setOnClickListener(View.OnClickListener { //Change Application level locale
+            LocaleHelper.setLocale(this, mLanguageCodeEn)
+            recreate()
+        })
+
+
     }
 }
+
+
