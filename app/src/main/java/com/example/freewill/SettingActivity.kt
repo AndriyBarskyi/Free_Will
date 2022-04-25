a57b5623eb91f6b07189067b8cd6fc49c52b0dd3
Julian Zaiats <YULIAN.ZAIATS@lnu.edu.ua>
2022-04-25 23:14:04 +0300

    selected language and outosave(#2)

--- app/src/main/java/com/example/freewill/SettingActivity.kt
+++ app/src/main/java/com/example/freewill/SettingActivity.kt
@@ -12,43 +12,76 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.freewill.databinding.ActivityScheduleBinding
+import com.example.freewill.databinding.ActivitySettingBinding
 import com.google.android.material.navigation.NavigationView
 
 
-class SettingActivity : AppCompatActivity() {
+class SettingActivity : AppCompatActivity()
+{
+    lateinit var bindingClass:ActivitySettingBinding
     private val mLanguageCodeEn = "en"
     private val mLanguageCodeUa = "ua"
     lateinit var chooseLang : String
+    val nameBaseLang = "LANGUAGE"
+    val path = "chooseLang"
     var resLang : SharedPreferences? = null
 
+    private lateinit var binding: ActivityScheduleBinding
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        resLang = getSharedPreferences("LANGUAGE", Context.MODE_PRIVATE)
-        chooseLang = resLang?.getString("chooseLang", mLanguageCodeUa)!!
+        resLang = getSharedPreferences(nameBaseLang, Context.MODE_PRIVATE)
+        chooseLang = resLang?.getString( path, mLanguageCodeUa)!!
         LocaleHelper.setLocale(this, chooseLang)
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting)
+        bindingClass = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
 
 
-        val a : Button = findViewById(R.id.english_language)
-        val b : Button = findViewById(R.id.ukraine_language)
-
-        b.setOnClickListener(View.OnClickListener { //Change Application level locale
+        bindingClass.ukraineLanguage.setOnClickListener(View.OnClickListener
+        { //Change Application level locale
             LocaleHelper.setLocale(this, mLanguageCodeUa)
             chooseLang = mLanguageCodeUa
             recreate()
         })
 
-        a.setOnClickListener(View.OnClickListener { //Change Application level locale
+        bindingClass.englishLanguage.setOnClickListener(View.OnClickListener
+        { //Change Application level locale
             LocaleHelper.setLocale(this, mLanguageCodeEn)
             chooseLang = mLanguageCodeEn
             recreate()
         })
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        val navView: NavigationView = findViewById(R.id.navView)
+        navView.itemIconTintList = null
+
+        navView.setNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.nav_schedule -> startActivity(Intent(this, ScheduleActivity::class.java))
+
+                R.id.nav_map -> startActivity(Intent(this, MapActivity::class.java))
+
+                R.id.nav_settings -> startActivity(Intent(this, SettingActivity::class.java))
+
+            }
+            true
+        }
     }
 
     fun saveLanguage(res:String)
     {
         val editor = resLang?.edit()
-        editor?.putString("chooseLang", res)
+        editor?.putString(path, res)
         editor?.apply()
 
     }
