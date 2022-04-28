2fa0b1e96a800e19cfbe1f6e186d59941495d8d6
Julian Zaiats <YULIAN.ZAIATS@lnu.edu.ua>
2022-04-28 22:15:30 +0300

    add color changes for button language and replace text to hint on xml for editText

--- app/src/main/java/com/example/freewill/SettingActivity.kt
+++ app/src/main/java/com/example/freewill/SettingActivity.kt
@@ -1,13 +1,13 @@
 package com.example.freewill
 
+
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.SharedPreferences
-import android.content.res.Configuration
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
@@ -18,31 +18,47 @@ import com.google.android.material.navigation.NavigationView
 
 class SettingActivity : AppCompatActivity()
 {
-    lateinit var bindingClass:ActivitySettingBinding
-    private val mLanguageCodeEn = "en"
-    private val mLanguageCodeUa = "ua"
-    lateinit var chooseLang : String
     val nameBaseLang = "LANGUAGE"
     val path = "chooseLang"
-    var resLang : SharedPreferences? = null
+    private val mLanguageCodeEn = "en"
+    private val mLanguageCodeUa = "ua"
 
+    lateinit var bindingClass: ActivitySettingBinding
+    lateinit var chooseLang : String
+    var resLang : SharedPreferences? = null
     private lateinit var binding: ActivityScheduleBinding
     lateinit var toggle: ActionBarDrawerToggle
     lateinit var drawerLayout: DrawerLayout
 
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         resLang = getSharedPreferences(nameBaseLang, Context.MODE_PRIVATE)
-        chooseLang = resLang?.getString( path, mLanguageCodeUa)!!
+        chooseLang = resLang?.getString(path, mLanguageCodeUa)!!
         LocaleHelper.setLocale(this, chooseLang)
 
         super.onCreate(savedInstanceState)
         bindingClass = ActivitySettingBinding.inflate(layoutInflater)
+
+        when(chooseLang){
+            mLanguageCodeEn->{bindingClass.ukraineLanguage.setBackgroundResource(R.color.less_blue)
+                bindingClass.englishLanguage.setBackgroundResource(R.color.dark_blue)}
+            mLanguageCodeUa->{ bindingClass.ukraineLanguage.setBackgroundResource(R.color.dark_blue)
+                bindingClass.englishLanguage.setBackgroundResource(R.color.less_blue)}
+        }
+
         setContentView(bindingClass.root)
+
+
 
 
         bindingClass.ukraineLanguage.setOnClickListener(View.OnClickListener
         { //Change Application level locale
             LocaleHelper.setLocale(this, mLanguageCodeUa)
+            val myToast = Toast.makeText(this,
+                "Мова змінена на українську. Перезагрузіть додаток.", Toast.LENGTH_SHORT)
+            myToast.show()
             chooseLang = mLanguageCodeUa
             recreate()
         })
@@ -50,10 +66,33 @@ class SettingActivity : AppCompatActivity()
         bindingClass.englishLanguage.setOnClickListener(View.OnClickListener
         { //Change Application level locale
             LocaleHelper.setLocale(this, mLanguageCodeEn)
+            val myToast = Toast.makeText(this, "language changed to english. Restart app.", Toast.LENGTH_SHORT)
+            myToast.show()
             chooseLang = mLanguageCodeEn
             recreate()
         })
 
+        toolBar()
+
+    }
+
+    fun smallFont(view:View)
+    {
+        bindingClass.buttonM.setBackgroundResource(R.color.less_blue)
+        bindingClass.buttonB.setBackgroundResource(R.color.less_blue)
+    }
+    fun mediumFont(view:View)
+    {
+
+    }
+    fun bigFont(view:View)
+    {
+
+    }
+
+
+    fun toolBar()
+    {
         drawerLayout = findViewById(R.id.drawerLayout)
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
 
@@ -76,6 +115,7 @@ class SettingActivity : AppCompatActivity()
             }
             true
         }
+
     }
 
     fun saveLanguage(res:String)
