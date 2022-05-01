843634a52fa81aab538329527ebbf49819219465
Julian Zaiats <YULIAN.ZAIATS@lnu.edu.ua>
2022-05-01 13:13:10 +0300

    fix strings.xml and select language

--- app/src/main/java/com/example/freewill/WelcomeActivity.kt
+++ app/src/main/java/com/example/freewill/WelcomeActivity.kt
@@ -1,14 +1,20 @@
 package com.example.freewill
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
 class WelcomeActivity : AppCompatActivity() {
+    lateinit var chooseLang : String
 
     private val SPLASH_TIME: Long = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
+        val a = SettingActivity()
+        a.resLang = getSharedPreferences(a.baseForSetting, Context.MODE_PRIVATE)
+        LocaleHelper.setLocale(this, a.resLang?.getString(a.keyLanguage, a.mLanguageCodeUa)!!)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
