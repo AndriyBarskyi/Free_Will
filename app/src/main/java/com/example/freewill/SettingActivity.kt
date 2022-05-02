5417f5ee36b9c4d80bf10d2757ad55734ca21cb9
Julian Zaiats <YULIAN.ZAIATS@lnu.edu.ua>
2022-05-02 15:47:38 +0300

    add text size

--- app/src/main/java/com/example/freewill/SettingActivity.kt
+++ app/src/main/java/com/example/freewill/SettingActivity.kt
@@ -5,10 +5,9 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
-import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -23,20 +22,38 @@ class SettingActivity : AppCompatActivity()
     val baseForSetting = "LANGUAGE"
     val keyLanguage = "chooseLang"
     val keyFont ="font"
-    private val mLanguageCodeEn = "en"
+    val mLanguageCodeEn = "en"
     val mLanguageCodeUa = "ua"
-    private val small = "small"
-    private val medium = "medium"
-    private val big = "big"
+    val small = "small"
+    val medium = "medium"
+    val big = "big"
+    val smallSize = 0.50f
+    val mediumSize = 1.0f
+    val bigSize = 1.6f
 
+    var chooseSizeKoef : Float? = null
+    var check :Boolean?=null
     lateinit var chooseFont : String
     lateinit var bindingClass: ActivitySettingBinding
-    public lateinit var chooseLang : String
+    lateinit var chooseLang : String
     var resLang : SharedPreferences? = null
     private lateinit var binding: ActivityScheduleBinding
     lateinit var toggle: ActionBarDrawerToggle
     lateinit var drawerLayout: DrawerLayout
 
+
+    fun SetSizeFont(size_coef: Float)
+    {
+        val configuration = resources.configuration
+        configuration.fontScale = size_coef //0.85 small size, 1 normal size, 1,15 big etc
+
+
+        val metrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(metrics)
+        metrics.scaledDensity = configuration.fontScale * metrics.density
+        baseContext.resources.updateConfiguration(configuration, metrics)
+
+    }
 
     @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,13 +63,11 @@ class SettingActivity : AppCompatActivity()
         LocaleHelper.setLocale(this, chooseLang)
 
         super.onCreate(savedInstanceState)
-
-
         bindingClass = ActivitySettingBinding.inflate(layoutInflater)
 
+
         // language selection after restart
-        when(chooseLang)
-        {
+        when(chooseLang) {
             mLanguageCodeEn->{bindingClass.ukraineLanguage.setBackgroundResource(R.color.less_blue)
                 bindingClass.englishLanguage.setBackgroundResource(R.color.dark_blue)}
             mLanguageCodeUa->{ bindingClass.ukraineLanguage.setBackgroundResource(R.color.dark_blue)
@@ -61,16 +76,23 @@ class SettingActivity : AppCompatActivity()
 
         // font size selection after restart
         chooseFont = resLang?.getString(keyFont, medium)!!
-        when(chooseFont)
-        {
-            small ->{selectColorsFontSize(R.drawable.circle_button_dark_blue, R.drawable.circle_button,
-                R.drawable.circle_button)}
-            medium ->{selectColorsFontSize(R.drawable.circle_button, R.drawable.circle_button_dark_blue,
-                R.drawable.circle_button)}
-            big ->{selectColorsFontSize(R.drawable.circle_button, R.drawable.circle_button,
-                R.drawable.circle_button_dark_blue)}
+        when(chooseFont) {
+            small ->{
+                selectColorsFontSize(R.drawable.circle_button_dark_blue, R.drawable.circle_button,
+                    R.drawable.circle_button)
+                SetSizeFont(smallSize)
+            }
+            medium ->{
+                selectColorsFontSize(R.drawable.circle_button, R.drawable.circle_button_dark_blue,
+                    R.drawable.circle_button)
+                SetSizeFont(mediumSize)
+            }
+            big ->{
+                selectColorsFontSize(R.drawable.circle_button, R.drawable.circle_button,
+                    R.drawable.circle_button_dark_blue)
+                SetSizeFont(bigSize)
+            }
         }
-
 
         setContentView(bindingClass.root)
 
@@ -96,29 +118,33 @@ class SettingActivity : AppCompatActivity()
         bindingClass.buttonS.setOnClickListener(View.OnClickListener
         {
             chooseFont = small
-            selectColorsFontSize(R.drawable.circle_button_dark_blue, R.drawable.circle_button,
-                R.drawable.circle_button)
-            recreate()
+            chooseSizeKoef = smallSize
+            SetSizeFont(smallSize)
+           recreate()
         })
         bindingClass.buttonM.setOnClickListener(View.OnClickListener
         {
             chooseFont = medium
-            selectColorsFontSize(R.drawable.circle_button, R.drawable.circle_button_dark_blue,
-                R.drawable.circle_button)
+            chooseSizeKoef = mediumSize
+            SetSizeFont(mediumSize)
             recreate()
+
         })
         bindingClass.buttonB.setOnClickListener(View.OnClickListener
         {
             chooseFont = big
-            selectColorsFontSize(R.drawable.circle_button, R.drawable.circle_button,
-                R.drawable.circle_button_dark_blue)
+            chooseSizeKoef = bigSize
+            SetSizeFont(bigSize)
             recreate()
+
         })
 
         //Navigation drawer
         NavigationDrawer()
 
     }
+
+
 
     fun saveLanguageAndFont(resLanguage:String, resFont:String)
     {
--- app/src/main/java/com/example/freewill/WelcomeActivity.kt
+++ app/src/main/java/com/example/freewill/WelcomeActivity.kt
@@ -2,26 +2,69 @@ package com.example.freewill
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.DisplayMetrics
 
 class WelcomeActivity : AppCompatActivity() {
-    lateinit var chooseLang : String
 
     private val SPLASH_TIME: Long = 3000
-    override fun onCreate(savedInstanceState: Bundle?) {
-        val a = SettingActivity()
+    val a = SettingActivity()
+    var checking : SharedPreferences? = null
+    val editor = checking?.edit()
+    val checkk = "check"
+
+    fun toSettingApp()
+    {
         a.resLang = getSharedPreferences(a.baseForSetting, Context.MODE_PRIVATE)
         LocaleHelper.setLocale(this, a.resLang?.getString(a.keyLanguage, a.mLanguageCodeUa)!!)
-
+        when(a.resLang?.getString(a.keyFont, a.medium)!!){
+            a.small->{SetSizeFont(a.smallSize)}
+            a.medium->{SetSizeFont(a.mediumSize)}
+            a.big->{SetSizeFont(a.bigSize) }
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        toSettingApp()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+/*
+        checking = getSharedPreferences("forSett", Context.MODE_PRIVATE)
+        val choose = checking?.getBoolean(checkk, true)!!
+        if (choose){
+            when(a.resLang?.getString(a.keyFont, a.medium)!!){
+                a.small->{SetSizeFont(a.smallSize)}
+                a.medium->{SetSizeFont(a.mediumSize)}
+                a.big->{SetSizeFont(a.bigSize)}
+            }
+            editor?.putBoolean(checkk, false)
+            editor?.apply()
+        }
+        editor?.putBoolean(checkk, true)
+        editor?.apply()
+*/
+
+
 
         Handler().postDelayed({
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }, SPLASH_TIME)
+
+    }
+
+    fun SetSizeFont(size_coef: Float)
+    {
+        val configuration = resources.configuration
+        configuration.fontScale = size_coef //0.85 small size, 1 normal size, 1,15 big etc
+
+
+        val metrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(metrics)
+        metrics.scaledDensity = configuration.fontScale * metrics.density
+        baseContext.resources.updateConfiguration(configuration, metrics)
 
     }
 }
