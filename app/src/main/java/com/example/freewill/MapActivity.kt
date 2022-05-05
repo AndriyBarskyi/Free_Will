65f8584e4e194519c4493af7c860b655f27a585d
barskyiandrii <barskyiandriy@gmail.com>
2022-05-05 22:05:58 +0300

    put FAB onClick func to the onCreate function

--- app/src/main/java/com/example/freewill/MapActivity.kt
+++ app/src/main/java/com/example/freewill/MapActivity.kt
@@ -10,10 +10,7 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.PopupWindow
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -23,6 +20,7 @@ import androidx.fragment.app.DialogFragment
 import com.example.freewill.databinding.ActivityMapBinding
 import com.example.freewill.search_point.Dijkstra
 import com.example.freewill.models.NavigationClass
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.textfield.TextInputEditText
 import java.util.ArrayList
@@ -33,27 +31,54 @@ class MapActivity : AppCompatActivity() {
     lateinit var toggle: ActionBarDrawerToggle
     lateinit var drawerLayout: DrawerLayout
     lateinit var mapBinding: ActivityMapBinding
-    var toPlacePoint: String = ""
-    var fromPlacePoint: String = ""
 
+    @SuppressLint("InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mapBinding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(mapBinding.root)
-        val text10: TextInputEditText = drawerLayout.findViewById(R.id.textView10)
-        if (toPlacePoint != ""){
-            text10.setText(toPlacePoint)
-        }
         createNavigationMenu()
+
+        val findDirectionButton: FloatingActionButton = mapBinding.findDirectionButton
+        findDirectionButton.setOnClickListener {
+            val inflater =
+                it.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            drawerLayout = findViewById(R.id.drawerLayout)
+            drawerLayout.foreground.alpha = 255
+            val popupView: View = inflater.inflate(R.layout.activity_search_popup, null)
+            val width = LinearLayout.LayoutParams.MATCH_PARENT
+            val height = LinearLayout.LayoutParams.MATCH_PARENT
+            val focusable = true
+            val popupWindow = PopupWindow(popupView, width, height, focusable)
+
+            popupWindow.showAtLocation(it, Gravity.CENTER, 0, 0)
+            popupWindow.isOutsideTouchable = true
+            popupWindow.setOnDismissListener {
+                drawerLayout.foreground.alpha = 0
+            }
+
+            val searchFromToButton: Button =
+                popupWindow.contentView.findViewById(R.id.searchFromToButton)
+            searchFromToButton.setOnClickListener {
+                var fromPlace: String = ""
+                var toPlace: String = ""
+                val searchFromInput: TextInputEditText =
+                    popupWindow.contentView.findViewById(R.id.searchFromInput)
+                fromPlace = searchFromInput.text.toString()
+
+                val searchToInput: TextInputEditText =
+                    popupWindow.contentView.findViewById(R.id.searchToInput)
+                toPlace = searchToInput.text.toString()
+                if (toPlace != "") {
+                    val text10: TextView = mapBinding.textView10
+                    text10.text = toPlace
+                }
+                popupWindow.dismiss()
+            }
+        }
     }
 
-    @JvmName("setToPlace1")
-    fun setFromAndToPlacePoints(from: String, to: String){
-        fromPlacePoint = from
-        toPlacePoint = to
-    }
-
-    fun createNavigationMenu(){
+    fun createNavigationMenu() {
         // assigning ID of the toolbar to a variable
         val toolbar: Toolbar = mapBinding.toolbar
 
@@ -63,56 +88,32 @@ class MapActivity : AppCompatActivity() {
 
         drawerLayout = findViewById(R.id.drawerLayout)
         drawerLayout.foreground.alpha = 0
-        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
+        toggle =
+            ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
         supportActionBar?.setDisplayShowHomeEnabled(true)
         val navView: NavigationView = findViewById(R.id.navView)
         val navigation = NavigationClass(drawerLayout, toggle, navView, this)
         navigation.createNavigationDrawer()
+
     }
 
-    @SuppressLint("ClickableViewAccessibility", "InflateParams")
-    fun onClickOpenPopUp(view:View) {
-        mapBinding = ActivityMapBinding.inflate(layoutInflater)
-        val inflater =
-            view.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        drawerLayout = findViewById(R.id.drawerLayout)
-        drawerLayout.foreground.alpha = 255
-        val popupView: View = inflater.inflate(R.layout.activity_search_popup, null)
-        val width = LinearLayout.LayoutParams.MATCH_PARENT
-        val height = LinearLayout.LayoutParams.MATCH_PARENT
-        val focusable = true
-        val popupWindow = PopupWindow(popupView, width, height, focusable)
+/*    @SuppressLint("ClickableViewAccessibility", "InflateParams")
+    fun onClickOpenPopUp(view: View) {
 
-        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
-        popupWindow.isOutsideTouchable = true
-        popupWindow.setOnDismissListener {
-            drawerLayout.foreground.alpha = 0
-        }
-
-        val findDirectionBtn: Button = popupWindow.contentView.findViewById(R.id.searchFromToButton)
-        findDirectionBtn.setOnClickListener {
-            var fromPlace: String = ""
-            var toPlace: String = ""
-            val searchFromInput: TextInputEditText = popupWindow.contentView.findViewById(R.id.searchFromInput)
-            fromPlace = searchFromInput.text.toString()
-
-            val searchToInput: TextInputEditText = popupWindow.contentView.findViewById(R.id.searchToInput)
-            toPlace = searchToInput.text.toString()
-            setFromAndToPlacePoints(fromPlace, toPlace)
             popupWindow.dismiss()
-/*            if (fromPlace != "" && toPlace != "") {
+*//*            if (fromPlace != "" && toPlace != "") {
                 val points: ArrayList<Int>? = Dijkstra.Calculate(fromPlace, toPlace, this.baseContext)
                 if (points != null) {
                     for (i in points){
                         Log.d("Point123", i.toString())
                     }
                 }
-            }*/
+            }*//*
         }
-    }
-    class InfoRoomFragment (_room : String): DialogFragment()
-    {
-        val room=_room
+    }*/
+
+    class InfoRoomFragment(_room: String) : DialogFragment() {
+        val room = _room
         override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
             return activity?.let {
                 val builder = AlertDialog.Builder(it)
@@ -122,71 +123,71 @@ class MapActivity : AppCompatActivity() {
             } ?: throw IllegalStateException("Activity cannot be null")
         }
     }
-    fun aud237(view: View)
-    {
+
+    fun aud237(view: View) {
         //Toast.makeText(this,"Aud 237 Information", Toast.LENGTH_SHORT).show()
         val infoRoomFragment = InfoRoomFragment("237")
         val manager = supportFragmentManager
         infoRoomFragment.show(manager, "myDialog")
     }
-    fun aud238(view: View)
-    {
+
+    fun aud238(view: View) {
         //Toast.makeText(this,"Aud 237 Information", Toast.LENGTH_SHORT).show()
         val infoRoomFragment = InfoRoomFragment("238")
         val manager = supportFragmentManager
         infoRoomFragment.show(manager, "myDialog")
     }
-    fun aud239(view: View)
-    {
+
+    fun aud239(view: View) {
         //Toast.makeText(this,"Aud 237 Information", Toast.LENGTH_SHORT).show()
         val infoRoomFragment = InfoRoomFragment("237")
         val manager = supportFragmentManager
         infoRoomFragment.show(manager, "myDialog")
     }
-    fun aud240(view: View)
-    {
+
+    fun aud240(view: View) {
         //Toast.makeText(this,"Aud 237 Information", Toast.LENGTH_SHORT).show()
         val infoRoomFragment = InfoRoomFragment("237")
         val manager = supportFragmentManager
         infoRoomFragment.show(manager, "myDialog")
     }
-    fun aud241(view: View)
-    {
+
+    fun aud241(view: View) {
         //Toast.makeText(this,"Aud 237 Information", Toast.LENGTH_SHORT).show()
         val infoRoomFragment = InfoRoomFragment("237")
         val manager = supportFragmentManager
         infoRoomFragment.show(manager, "myDialog")
     }
-    fun aud241a(view: View)
-    {
+
+    fun aud241a(view: View) {
         //Toast.makeText(this,"Aud 237 Information", Toast.LENGTH_SHORT).show()
         val infoRoomFragment = InfoRoomFragment("237")
         val manager = supportFragmentManager
         infoRoomFragment.show(manager, "myDialog")
     }
-    fun aud242(view: View)
-    {
+
+    fun aud242(view: View) {
         //Toast.makeText(this,"Aud 237 Information", Toast.LENGTH_SHORT).show()
         val infoRoomFragment = InfoRoomFragment("237")
         val manager = supportFragmentManager
         infoRoomFragment.show(manager, "myDialog")
     }
-    fun aud243(view: View)
-    {
+
+    fun aud243(view: View) {
         //Toast.makeText(this,"Aud 237 Information", Toast.LENGTH_SHORT).show()
         val infoRoomFragment = InfoRoomFragment("237")
         val manager = supportFragmentManager
         infoRoomFragment.show(manager, "myDialog")
     }
-    fun aud244(view: View)
-    {
+
+    fun aud244(view: View) {
         //Toast.makeText(this,"Aud 237 Information", Toast.LENGTH_SHORT).show()
         val infoRoomFragment = InfoRoomFragment("237")
         val manager = supportFragmentManager
         infoRoomFragment.show(manager, "myDialog")
     }
-    fun aud245(view: View)
-    {
+
+    fun aud245(view: View) {
         //Toast.makeText(this,"Aud 237 Information", Toast.LENGTH_SHORT).show()
         val infoRoomFragment = InfoRoomFragment("237")
         val manager = supportFragmentManager
