a9661d0a46776b9ca6de8eb4185cc3e2af0e226d
barskyiandrii <barskyiandriy@gmail.com>
2022-05-17 15:27:28 +0300

    update

--- app/src/main/java/com/example/freewill/models/NavigationClass.kt
+++ app/src/main/java/com/example/freewill/models/NavigationClass.kt
@@ -63,8 +63,9 @@ class NavigationClass(
 
                 }
                 R.id.nav_feedback -> {
-//                    val i = Intent(context, SettingActivity::class.java)
-//                    context.startActivity(i)
+                    val i = Intent(context, ReviewActivity::class.java)
+                    context.startActivity(i)
+                    activityClass.finish()
 
                 }
                 R.id.nav_useful_links -> {
