b90a011dd11713e22da7b4297f0dcc6f6449c5a3
KseniaGovorun <ksena.govorun@gmail.com>
2022-05-17 14:50:22 +0300

    Done reciclerView with dataBase

--- app/src/main/java/Links/LinkFragment.kt
+++ app/src/main/java/Links/LinkFragment.kt
@@ -6,43 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.freewill.R
 import com.example.freewill.databinding.ActivityNewsBinding
 import com.example.freewill.databinding.FragmentLinkBinding
 
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 class LinkFragment : Fragment() {
-
-    private var param1: String? = null
-    private var param2: String? = null
     private lateinit var binding: FragmentLinkBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val adapter = LinksAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_link, container, false)
+        binding = FragmentLinkBinding.inflate(layoutInflater)
+        init()
+        return binding.root
+    }
+    private fun init(){
+        binding.apply {
+            recView.layoutManager=LinearLayoutManager(activity)
+            recView.adapter = adapter
+            adapter.AddLinks("C++")
+        }
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LinkFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
--- app/src/main/java/Links/Links.kt
+++ app/src/main/java/Links/Links.kt
@@ -1,3 +1,4 @@
 package Links
 
 data class Links(val title: String)
+{}
