9918991acfaed8da0dd232bef870961b41ebd79d
Oksana-Olesja <oksanavilchinska2003@gmail.com>
2022-05-18 14:43:15 +0300

    add edit_audience, add toolbar and navigation in LinkPreview

--- app/src/main/java/Links/LinksPreviewActivity.kt
+++ app/src/main/java/Links/LinksPreviewActivity.kt
@@ -7,12 +7,22 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.freewill.R
+import com.example.freewill.databinding.ActivityLinksPreviewBinding
+import com.example.freewill.databinding.ActivityNewsBinding
+import com.example.freewill.models.NavigationClass
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 class LinksPreviewActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLinksPreviewBinding
     private lateinit var referenceSubject: DatabaseReference
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
     fun readSubjects(onSuccess:(Array<String>)->Unit) {
         referenceSubject = FirebaseDatabase
             .getInstance("https://freewilldatabase-default-rtdb.europe-west1.firebasedatabase.app/")
@@ -35,8 +45,24 @@ class LinksPreviewActivity : AppCompatActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLinksPreviewBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_links_preview)
         readSubjects(){array->CreateItem(array)}
+
+        // assigning ID of the toolbar to a variable
+        val toolbar: Toolbar = binding.toolbar
+
+        // using toolbar as ActionBar
+        toolbar.setTitle(R.string.toolbar_link)
+        setSupportActionBar(toolbar)
+
+//      Navigation bar
+        drawerLayout = findViewById(R.id.drawerLayout)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        val navView: NavigationView = findViewById(R.id.navView)
+        val navigation = NavigationClass(drawerLayout, toggle, navView, this)
+        navigation.createNavigationDrawer(this)
     }
     fun CreateItem(languages:Array<String>)
     {
--- app/src/main/java/com/example/freewill/NewsActivity.kt
+++ app/src/main/java/com/example/freewill/NewsActivity.kt
@@ -4,18 +4,25 @@ import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.freewill.databinding.ActivityLoginBinding
 import com.example.freewill.databinding.ActivityNewsBinding
+import com.example.freewill.models.NavigationClass
 import com.example.freewill.models.NewsAdapter
 import com.example.freewill.models.NewsModel
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.database.*
 
 class NewsActivity : AppCompatActivity() {
 
 //    private lateinit var recyclerView: RecyclerView
     private lateinit var binding: ActivityNewsBinding
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
     private lateinit var database: FirebaseDatabase
     private lateinit var databaseReference: DatabaseReference
     private lateinit var newsList: ArrayList<NewsModel>
@@ -38,6 +45,22 @@ class NewsActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
 //        recyclerView.setHasFixedSize(true)
 //        recyclerView.adapter = newsAdapter
+
+
+        // assigning ID of the toolbar to a variable
+        val toolbar: Toolbar = binding.toolbar
+
+        // using toolbar as ActionBar
+        toolbar.setTitle(R.string.toolbar_schedule)
+        setSupportActionBar(toolbar)
+
+//      Navigation bar
+        drawerLayout = findViewById(R.id.drawerLayout)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        val navView: NavigationView = findViewById(R.id.navView)
+        val navigation = NavigationClass(drawerLayout, toggle, navView, this)
+        navigation.createNavigationDrawer(this)
     }
 
 
