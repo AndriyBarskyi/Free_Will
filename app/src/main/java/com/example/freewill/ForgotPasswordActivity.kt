c30037e0860c960e3c3b2475f2c0520c3b539add
Kron285 <91598400+Kron285@users.noreply.github.com>
2022-05-24 00:16:19 +0300

    final update forgotPasswordActivity

--- app/src/main/java/com/example/freewill/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/freewill/ForgotPasswordActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.util.Patterns
 import android.widget.Toast
 import com.example.freewill.databinding.ActivityForgotPasswordBinding
-import com.example.freewill.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class ForgotPasswordActivity : AppCompatActivity() {
@@ -20,6 +19,7 @@ class ForgotPasswordActivity : AppCompatActivity() {
         binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //resetButton is clicked
         binding.resetPasswordButton.setOnClickListener {
             resetPassword()
             startActivity(Intent(this, LoginActivity::class.java))
@@ -27,6 +27,7 @@ class ForgotPasswordActivity : AppCompatActivity() {
         }
     }
 
+    //reset user password with Firebase
     private fun resetPassword(){
         val emailText = binding.emailEditText.text.toString().trim()
 
